795394024ae371ac947b74306e40a1fa3b16870b
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-09-15 19:14:33 +0530

    Project started

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <main>
+      
+      <div
+        className="fixed top-0 left-0 w-screen h-screen bg-cover bg-center overflow-y-auto"
+        style={{ backgroundImage: "url('law1.jpeg')" , zIndex: "-1"}}
+  
+      >
+
+      </div>
+      </main>
+     
+    
+  );
+}
